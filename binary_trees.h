b3048af1a024c01f7d5f64643726a41f893883a4
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-11-30 13:32:19 -0500

    [UPDATE] Include stdlib.h

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,9 @@
 #ifndef H_BIN_TREE
 #define H_BIN_TREE
 
+#include <stdlib.h>
+#include <stddef.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -44,5 +47,6 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+void binary_tree_print(const binary_tree_t *);
 
 #endif /* H_BIN_TREE */
