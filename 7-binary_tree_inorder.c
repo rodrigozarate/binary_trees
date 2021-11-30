7c505b0161ed13294dc6c9580af0eedbce6b509f
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-11-30 16:39:26 -0500

    [CREATE]

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,22 @@
+/*
+* 7-binary_tree_inorder.c - Goes through binary tree using in-order transversal
+* Author: Juan Sebastian Tovar Mogollon and Rodrigo Zárate Algecira
+* Date: November 30, 2021
+*/
+
+/**
+* binary_tree_inorder - Goes through binary tree using in-order transversal
+* @tree: pointer to Binary
+* @func: void pointer to function
+*/
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL)
+		return;
+	if (func == NULL)
+		return;
+
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
+}
