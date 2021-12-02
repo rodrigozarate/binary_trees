0d888b62664d5313957564c0e6d69ba867d0ca22
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-12-01 22:42:15 -0500

    [FIX]

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -15,14 +15,14 @@
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-        if (node == NULL)
-                return (0);
+	if (node == NULL)
+		return (0);
 
-        if (node->left == NULL && node->right == NULL)
-                return (1);
+	if (node->left == NULL && node->right == NULL)
+		return (1);
 
-        else
-                return (0);
+	else
+		return (0);
 } 
 
 /**
